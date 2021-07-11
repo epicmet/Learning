2f4be6ff0842fc014d5dfceb0ca8a0551791f6f8
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-11 19:59:48 +0430

    made basic boilerplate of project

---
+++ React/random-preson/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
