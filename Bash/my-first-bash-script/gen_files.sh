5e9e446374329878205114e998669967962aaa35
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-12 16:45:08 +0430

    dealing with variables

--- Bash/my-first-bash-script/gen_files.sh
+++ Bash/my-first-bash-script/gen_files.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-mkdir -p ./temp
-cd ./temp
-touch file{1..10}.txt
+DESTINATION=./temp
+FILE_PREFIX=file
+
+mkdir -p $DESTINATION
+cd $DESTINATION
+touch ${FILE_PREFIX}{1..10}.txt
 echo done
 
