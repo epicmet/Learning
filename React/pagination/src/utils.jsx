a4e43c665d311245d4a2177e3359b2d55eaee5a7
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-12 15:20:28 +0430

    made paginating logic

--- React/pagination/src/utils.jsx
+++ React/pagination/src/utils.jsx
@@ -1,3 +1,20 @@
-const paginate = () => {};
+const paginate = (followers) => {
+  const itemsPerPage = 10;
+
+  const pages = Math.ceil(followers.length / itemsPerPage);
+
+  //   const newFollowers = [];
+  //   for (let i = 0; i < followers.length; i += itemsPerPage) {
+  //     const newArr = followers.slice(i, i + itemsPerPage);
+  //     newFollowers.push(newArr);
+  //   }
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+
+  return newFollowers;
+};
 
 export default paginate;
