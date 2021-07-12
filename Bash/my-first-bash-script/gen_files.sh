f26bc29d8351ce3d473e2e65fd661280d3956372
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-12 17:14:26 +0430

    defined default value for destination with if statement

--- Bash/my-first-bash-script/gen_files.sh
+++ Bash/my-first-bash-script/gen_files.sh
@@ -3,6 +3,11 @@
 DESTINATION=$1
 read -p "please enter a fileprefix: " FILE_PREFIX
 
+if [ -z $DESTINATION ]; then
+	echo "no path provided, defaulting to ./temp"
+	DESTINATION=temp
+fi
+
 mkdir -p $DESTINATION
 cd $DESTINATION
 touch ${FILE_PREFIX}{1..10}.txt
