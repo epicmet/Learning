6133c4adb9b300edf72f0ff6d0ae9304f4406207
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-13 17:04:15 +0430

    set photo component for showing fetched photos

--- React/stock-photos/src/App.jsx
+++ React/stock-photos/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
-// import Photo from "./Photo";s
+import Photo from "./Photo";
 const clientID = `?client_id=${import.meta.env.VITE_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
@@ -17,18 +17,42 @@ function App() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      setPhotos(data);
+      setLoading(false);
     } catch (e) {
       setLoading(false);
       console.log(e);
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
     fetchImages();
   }, []);
 
-  return <h2>stock photos starter</h2>;
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder="search" className="form-input" />
+          <button className="submit-btn" type="submit" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading ...</h2>}
+      </section>
+    </main>
+  );
 }
 
 export default App;
