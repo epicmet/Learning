64671faa143270fcc5bc5820bef24d1bca44797b
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-18 22:41:55 +0430

    made each story layout

--- React/hacker-news/src/Stories.jsx
+++ React/hacker-news/src/Stories.jsx
@@ -2,13 +2,44 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits, removeStory } = useGlobalContext();
 
   if (isLoading) {
     return <div className="loading"></div>;
   }
 
-  return <h1>Stories</h1>;
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const { objectID, title, num_comments, url, points, author } = story;
+        return (
+          <article key={objectID} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span> {num_comments}{" "}
+              commnets
+            </p>
+            <div>
+              <a
+                href={url}
+                className="read-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                read more
+              </a>
+              <button
+                className="remove-btn"
+                onClick={() => removeStory(objectID)}
+              >
+                remove
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stories;
