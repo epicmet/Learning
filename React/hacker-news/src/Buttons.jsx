87ac5fe1341ecf8ca6df7f8ad22c61df45549cea
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-19 00:03:50 +0430

    made prev and next buttons

--- React/hacker-news/src/Buttons.jsx
+++ React/hacker-news/src/Buttons.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useGlobalContext } from "./context";
 
 const Buttons = () => {
-  return <h1>Buttons</h1>;
+  const { handlePage, page, nbPages, isLoading } = useGlobalContext();
+
+  return (
+    <div className="btn-container">
+      <button onClick={() => handlePage("dec")} disabled={isLoading}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button onClick={() => handlePage("inc")} disabled={isLoading}>
+        next
+      </button>
+    </div>
+  );
 };
 
 export default Buttons;
--- React/hacker-news/src/reducer.js
+++ React/hacker-news/src/reducer.js
@@ -24,6 +24,16 @@ const reducer = (state, action) => {
       };
     case HANDLE_SEARCH:
       return { ...state, query: action.payload, page: 0 };
+    case HANDLE_PAGE:
+      if (action.payload === "inc") {
+        let nextPage = state.page + 1;
+        if (nextPage > state.nbPages - 1) nextPage = 0;
+        return { ...state, page: nextPage };
+      } else if (action.payload === "dec") {
+        let prevPage = state.page - 1;
+        if (prevPage < 0) prevPage = state.nbPages - 1;
+        return { ...state, page: prevPage };
+      }
     default:
       throw new Error(`No matching "${action.type}" action`);
   }
