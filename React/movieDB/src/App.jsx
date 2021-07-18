88f86d93cc65c1e9aca17d0f5ae3132d1150aedb
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-18 13:19:54 +0430

    api key setup

--- React/movieDB/src/App.jsx
+++ React/movieDB/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import { API_ENDPOINT } from "./context";
+
 function App() {
   return <h2>movie DB starter</h2>;
 }
--- React/movieDB/src/context.jsx
+++ React/movieDB/src/context.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useContext, useEffect } from "react";
 
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${
+  import.meta.env.VITE_API_KEY
+}`;
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
