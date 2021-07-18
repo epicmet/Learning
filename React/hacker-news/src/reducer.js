ff8d2d179698304bf9bbe54c0fe997fa26fc5dad
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-18 16:43:39 +0430

    made basic context for project

---
+++ React/hacker-news/src/reducer.js
@@ -0,0 +1,17 @@
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true };
+    default:
+      throw new Error(`No matching "${action.type}" action`);
+  }
+};
+export default reducer;
