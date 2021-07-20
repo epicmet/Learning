221b752654506f9313bb80861ff0b2df88b32b88
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-20 11:34:18 +0430

    made basic states and context for app

--- React/quiz/src/App.jsx
+++ React/quiz/src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useGlobalContext } from "./context";
 
+import SetupForm from "./SetupForm";
+import Loading from "./Loading";
+
 function App() {
-  return <h2>quiz starter</h2>;
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+
+  if (waiting) return <SetupForm />;
+  if (loading) return <Loading />;
+
+  return <main>quiz app</main>;
 }
 
 export default App;
