148df621cd96284bd02ce23aa343a844269f1751
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-20 12:05:55 +0430

    made basic layout for showing a single question

--- React/quiz/src/App.jsx
+++ React/quiz/src/App.jsx
@@ -3,6 +3,7 @@ import { useGlobalContext } from "./context";
 
 import SetupForm from "./SetupForm";
 import Loading from "./Loading";
+import Modal from "./Modal";
 
 function App() {
   const { waiting, loading, questions, index, correct } = useGlobalContext();
@@ -10,7 +11,34 @@ function App() {
   if (waiting) return <SetupForm />;
   if (loading) return <Loading />;
 
-  return <main>quiz app</main>;
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers: {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, i) => {
+              return (
+                <button
+                  key={i}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
