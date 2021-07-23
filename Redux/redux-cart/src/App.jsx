de63832535f1bd7aa62ead6c31b17ea25b579109
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-23 14:02:52 +0430

    made basic redux setup

--- Redux/redux-cart/src/App.jsx
+++ Redux/redux-cart/src/App.jsx
@@ -6,6 +6,36 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+import { createStore } from "redux";
+
+const initialStore = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  if (action.type === "DECREASE") {
+    return { ...state, count: state.count - 1 };
+  }
+
+  if (action.type === "INCREASE") {
+    return { ...state, count: state.count + 1 };
+  }
+
+  if (action.type === "RESET") {
+    return { ...state, count: 0 };
+  }
+
+  return state;
+};
+
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "RESET" });
+console.log(store.getState());
+
 function App() {
   // cart setup
 
