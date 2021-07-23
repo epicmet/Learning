32d771ee599f0abe852d531019d75dead69a3dd9
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-23 16:03:42 +0430

    fixed typo for get totals and added toggle_amount

--- Redux/redux-cart/src/actions.js
+++ Redux/redux-cart/src/actions.js
@@ -2,5 +2,6 @@ export const DECREASE = "DECREASE";
 export const INCREASE = "INCREASE";
 export const REMOVE = "REMOVE";
 export const CLEAR_CART = "CLEAR_CART";
-export const GET_TOTAL = "GET_TOTAL";
+export const GET_TOTALS = "GET_TOTALS";
 export const GET_AMOUNT = "GET_AMOUNT";
+export const TOGGLE_AMOUNT = "TOGGLE_AMOUNT";
