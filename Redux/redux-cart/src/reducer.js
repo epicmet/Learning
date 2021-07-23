0f5f39f9955600cc3922a2c21c141c22bbfe4043
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-23 16:02:17 +0430

    made increase and decrease actions

--- Redux/redux-cart/src/reducer.js
+++ Redux/redux-cart/src/reducer.js
@@ -2,11 +2,32 @@ import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
 
 const reducer = (state, action) => {
   if (action.type === DECREASE) {
-    return { ...state, count: state.count - 1 };
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter((item) => item.id !== action.payload.id);
+    } else {
+      tempCart = state.cart.map((item) => {
+        if (item.id === action.payload.id) {
+          item = { ...item, amount: (item.amount -= 1) };
+        }
+
+        return item;
+      });
+    }
+
+    return { ...state, cart: tempCart };
   }
 
   if (action.type === INCREASE) {
-    return { ...state, count: state.count + 1 };
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload.id) {
+        item = { ...item, amount: (item.amount += 1) };
+      }
+
+      return item;
+    });
+
+    return { ...state, cart: tempCart };
   }
 
   if (action.type === CLEAR_CART) {
