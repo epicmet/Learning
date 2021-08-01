37a5696b0f5ede794a6905e2eb51a1263c783c3e
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-01 17:49:05 +0430

    countUniqueValues

---
+++ Algorithm/countUniqueValues/solution_one.js
@@ -0,0 +1,26 @@
+const countUniqueValues = function (arr) {
+  let res = 1;
+
+  let i = 0;
+  let j = 1;
+
+  if (arr.length === 0) return undefined;
+
+  while (j < arr.length) {
+    if (arr[i] !== arr[j]) {
+      i = j;
+      j++;
+      res++;
+    } else {
+      j++;
+    }
+  }
+
+  console.log(res);
+};
+
+countUniqueValues([]);
+countUniqueValues([1, 1, 1, 1]);
+countUniqueValues([1, 1, 1, 1, 2, 2, 2, 2]);
+countUniqueValues([1, 1, 2, 2, 2, 3, 3, 5, 6]);
+countUniqueValues([-9, -9, -4, -4, -2, -1, 0, 1, 1, 4, 4, 6]);
