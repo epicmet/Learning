83577136335a188662e1ea3ccdb3da99297301c9
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-07 11:47:48 +0430

    mergeFunc

---
+++ Algorithm/mergeFunc/index.js
@@ -0,0 +1,28 @@
+const merge = (arr1, arr2) => {
+  let res = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      res.push(arr1[i]);
+      i++;
+    } else {
+      res.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    res.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    res.push(arr2[j]);
+    j++;
+  }
+
+  console.log(res);
+};
+
+merge([2, 4, 7, 8], [1, 2, 8, 9]);
