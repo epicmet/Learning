52762328cc83e13160627cedb1618f5a07d3a2bb
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-14 14:40:49 +0430

    made first server

---
+++ fullStackForFrontendDevs/server/simpleServer.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function (req,res){
+	res.write('Hello World');
+	res.end();
+}).listen(8080);
+
+console.log('Server started, listening on port 8080');
+
