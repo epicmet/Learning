6e7aa466d82525baca9700b6a1578d24a7472f02
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-03 18:08:25 +0430

    made basic layout for error page

--- React/github-users/src/pages/Error.js
+++ React/github-users/src/pages/Error.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, we could not find the page you looking for</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
