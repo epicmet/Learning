037533061bf44258e88f6e2a230b9a7d049019a0
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-04 15:39:35 +0430

    made doughnut chart

--- React/github-users/src/components/Charts/Doughnut2d.js
+++ React/github-users/src/components/Charts/Doughnut2d.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Starts Per Language",
+        theme: "candy",
+        decimals: 0,
+        doughnutRadius: "45%",
+        showPercentValues: 0,
+      },
+      // Chart Data
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Doughnut2d;
+export default ChartComponent;
--- React/github-users/src/components/Repos.js
+++ React/github-users/src/components/Repos.js
@@ -6,28 +6,41 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
 
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
     if (!language) return total;
 
-    if (!total[language]) total[language] = { label: language, value: 1 };
+    if (!total[language])
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     else
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
 
     return total;
   }, {});
 
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((a, b) => b.value - a.value)
     .slice(0, 5);
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => b.stars - a.stars)
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+  console.log(mostPopular);
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={languages} />
+        <Pie3D data={mostUsed} />
+        <div></div>
+        <Doughnut2D data={mostPopular} />
+        <div></div>
       </Wrapper>
     </section>
   );
