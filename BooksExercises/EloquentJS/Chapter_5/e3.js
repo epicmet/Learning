e0a51f33e8d3d546663e83e6af115cf8c1639227
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-07 21:25:41 +0430

    Chapter 5, e3

---
+++ BooksExercises/EloquentJS/Chapter_5/e3.js
@@ -0,0 +1,14 @@
+function every(array, predicate) {
+  for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
