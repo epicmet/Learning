b99c669d8204ae1fa4aa10313b875c40b8186502
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-16 16:51:29 +0430

    made use of pipe and compose from lodash

--- Redux/mosh-redux-course/src/index.js
+++ Redux/mosh-redux-course/src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+
+let input = "   JavaScript   ";
+
+const trim = (str) => sty.trim();
+const toLowerCase = (str) => str.toLowerCase();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+transform(input);
