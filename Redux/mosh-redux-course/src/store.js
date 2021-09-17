9981a459fd08b36e6e6afb8909b5f14cce69090a
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-17 17:27:31 +0430

    made use of devToolsEnhancer to enable trace

--- Redux/mosh-redux-course/src/store.js
+++ Redux/mosh-redux-course/src/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
+
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+const store = createStore(reducer, devToolsEnhancer({ trace: true }));
 
 export default store;
