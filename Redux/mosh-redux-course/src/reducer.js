47514a74455347c7f82a4164fa164036ed1a21d2
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-17 15:51:09 +0430

    fixed typo and change {} to []

--- Redux/mosh-redux-course/src/reducer.js
+++ Redux/mosh-redux-course/src/reducer.js
@@ -3,15 +3,17 @@ let lastId = 0;
 export default function reducer(state = [], action) {
   switch (action.type) {
     case "bugAdded":
-      return {
+      return [
         ...state,
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      };
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
 
     case "bugRemoved":
-      return state.filter((bug) => bug.id !== actoin.payload.id);
+      return state.filter((bug) => bug.id !== action.payload.id);
 
     default:
       return state;
