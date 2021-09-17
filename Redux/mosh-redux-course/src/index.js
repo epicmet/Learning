f66bf7aef0e90cf266eba3435ec224f536f60a6f
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-17 16:12:07 +0430

    made actions file to avoid typing dispatch every time

--- Redux/mosh-redux-course/src/index.js
+++ Redux/mosh-redux-course/src/index.js
@@ -1,24 +1,6 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded } from "./actions";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("State changed", store.getState());
-});
-
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1",
-  },
-});
-
-unsubscribe();
-
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugAdded("Bug1"));
 
 console.log(store.getState());
