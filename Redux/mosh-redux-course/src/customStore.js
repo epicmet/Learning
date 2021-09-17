b67caf623350edff64f9bd84c8df680fe0e61e20
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-17 16:49:35 +0430

    designed custom dispatch

--- Redux/mosh-redux-course/src/customStore.js
+++ Redux/mosh-redux-course/src/customStore.js
@@ -1,13 +1,20 @@
-function createStore() {
+import reducer from "./reducer";
+
+function createStore(reducer) {
   let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+  }
 
   function getState() {
     return state;
   }
 
   return {
+    dispatch,
     getState,
   };
 }
 
-export default createStore();
+export default createStore(reducer);
--- Redux/mosh-redux-course/src/index.js
+++ Redux/mosh-redux-course/src/index.js
@@ -1,3 +1,6 @@
 import store from "./customStore";
+import * as actions from "./actions";
 
-console.log(store);
+store.dispatch(actions.bugAdded("Bug1"));
+
+console.log(store.getState());
