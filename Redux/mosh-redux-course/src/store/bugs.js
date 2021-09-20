3b2c0035b8d82169bb10311a5f837b4dcaff9d74
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-20 15:43:22 +0430

    combined actionTypes, actions and reducer into bug.js

--- Redux/mosh-redux-course/src/store/bugs.js
+++ Redux/mosh-redux-course/src/store/bugs.js
@@ -1,10 +1,29 @@
-import * as actions from "./actionTypes";
+// Action types
+const BUG_ADDED = "bugAdded";
+const BUG_REMOVED = "bugRemoved";
+const BUG_RESOLVED = "bugResolved";
+
+// Action creator
+export const bugAdded = (description) => ({
+  type: BUG_ADDED,
+  payload: {
+    description,
+  },
+});
+
+export const bugResolved = (id) => ({
+  type: BUG_RESOLVED,
+  payload: {
+    id,
+  },
+});
+
+// Reducer
 
 let lastId = 0;
-
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case actions.BUG_ADDED:
+    case BUG_ADDED:
       return [
         ...state,
         {
@@ -14,10 +33,10 @@ export default function reducer(state = [], action) {
         },
       ];
 
-    case actions.BUG_REMOVED:
+    case BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id);
 
-    case actions.BUG_RESOLVED:
+    case BUG_RESOLVED:
       return state.map((bug) =>
         bug.id === action.payload.id ? { ...bug, resolved: true } : bug
       );
