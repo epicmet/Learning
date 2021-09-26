46dd9f46dd01a37099a02543309c3c5df1f5cb9f
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-26 15:39:15 +0330

    change initial state of bugs to an object

--- Redux/mosh-redux-course/src/store/bugs.js
+++ Redux/mosh-redux-course/src/store/bugs.js
@@ -4,10 +4,14 @@ import { createSelector } from "reselect";
 let lastId = 0;
 const slice = createSlice({
   name: "bugs",
-  initialState: [],
+  initialState: {
+    list: [],
+    loading: false,
+    lastFetch: null,
+  },
   reducers: {
     bugAdded: (bugs, action) => {
-      bugs.push({
+      bugs.list.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false,
@@ -15,14 +19,14 @@ const slice = createSlice({
     },
 
     bugResolved: (bugs, action) => {
-      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-      bugs[index].resolved = true;
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
+      bugs.list[index].resolved = true;
     },
 
     bugAssignedToUser: (bugs, action) => {
       const { userId, bugId } = action.payload;
-      const index = bugs.findIndex((bug) => bug.id === bugId);
-      bugs[index].userId = userId;
+      const index = bugs.list.findIndex((bug) => bug.id === bugId);
+      bugs.list[index].userId = userId;
     },
   },
 });
