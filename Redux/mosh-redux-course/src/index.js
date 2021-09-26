b8467148b18a24c64b934893cba65cfd01e70e8c
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-26 12:09:25 +0330

    made toast middleware (exercise)

--- Redux/mosh-redux-course/src/index.js
+++ Redux/mosh-redux-course/src/index.js
@@ -11,6 +11,7 @@ import { userAdded } from "./store/users";
 
 const store = configureStore();
 
-store.dispatch((dispatch, getState) => {
-  dispatch({ type: "bugRecived", bugs: [1, 2, 3] });
+store.dispatch({
+  type: "error",
+  payload: { message: "An error occurred." },
 });
--- Redux/mosh-redux-course/src/store/configureStore.js
+++ Redux/mosh-redux-course/src/store/configureStore.js
@@ -1,10 +1,10 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
-import logger from "./middleware/logger";
+import toast from "./middleware/toast";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger({ destination: "console" })],
+    middleware: [toast],
   });
 }
