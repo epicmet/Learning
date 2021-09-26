df9b203dee2b797d92abf6c10cb2f82d1b0516af
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-26 11:40:47 +0330

    made a very basic middleware

--- Redux/mosh-redux-course/src/index.js
+++ Redux/mosh-redux-course/src/index.js
@@ -17,13 +17,13 @@ store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(bugAdded({ description: "Bug1" }));
-store.dispatch(bugAdded({ description: "Bug2" }));
-store.dispatch(bugAdded({ description: "Bug3" }));
-store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugAdded({ description: "Bug1" }));
+// store.dispatch(bugAdded({ description: "Bug2" }));
+// store.dispatch(bugAdded({ description: "Bug3" }));
+// store.dispatch(bugResolved({ id: 1 }));
 
-store.dispatch(userAdded({ name: "John" }));
-store.dispatch(bugAssignedToUser({ userId: 1, bugId: 2 }));
+// store.dispatch(userAdded({ name: "John" }));
+// store.dispatch(bugAssignedToUser({ userId: 1, bugId: 2 }));
 
 const bugs = getBugsByUser(1)(store.getState());
 console.log(bugs);
