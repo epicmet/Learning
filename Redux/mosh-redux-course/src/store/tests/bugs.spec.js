e4e90c8469767f08504492dd326ad170ee98fd1c
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-27 18:02:13 +0330

    imporved test to be a unit test and do not call an api request

--- Redux/mosh-redux-course/src/store/tests/bugs.spec.js
+++ Redux/mosh-redux-course/src/store/tests/bugs.spec.js
@@ -1,11 +1,18 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import configureStore from "../configureStore";
 import { addBug } from "../bugs";
 
 describe("bugSlice", () => {
   it("should handle the addBug action", async () => {
-    const store = configureStore();
     const bug = { describtion: "a" };
-    const result = await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost("/bugs").reply(200, savedBug);
+
+    const store = configureStore();
+    await store.dispatch(addBug(bug));
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
