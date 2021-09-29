61cadb4ae1d3fbb5dd483fff9795a9561bf60027
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-29 18:04:05 +0330

    function types

---
+++ TypeScript/type-basics/app.ts
@@ -0,0 +1,13 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  console.log("Result: ", num);
+}
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+printResult(combineValues(8, 8));
