e72630a8a1b13fe180f4a0e800d497ad50ea682e
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-29 17:42:16 +0330

    literal types

--- TypeScript/type-basics/app.ts
+++ TypeScript/type-basics/app.ts
@@ -1,10 +1,19 @@
-function add(input1: number | string, input2: number | string) {
-  if (typeof input1 === "number" && typeof input2 === "number")
-    return input1 + input2;
-  else return input1.toString() + input2.toString();
+function add(
+  input1: number | string,
+  input2: number | string,
+  type: "is-number" | "is-string"
+) {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    type === "is-number"
+  ) {
+    return +input1 + +input2;
+  } else {
+    return input1.toString() + input2.toString();
+  }
 }
 
-const numbers = add(1, 3);
-const names = add("matt", "s");
+const numbers = add(1, 3, "is-number");
+const names = add("matt", "s", "is-string");
 
 console.log(numbers, names);
