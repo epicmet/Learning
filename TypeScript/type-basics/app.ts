c792fc6b36ea59570eb9faf59f2a5eed240d0ea4
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-29 15:05:16 +0330

    boolean and string types

--- TypeScript/type-basics/app.ts
+++ TypeScript/type-basics/app.ts
@@ -1,10 +1,11 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  if (showResult) console.log(phrase + (n1 + n2));
+  else return phrase + (n1 + n2);
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1, number2);
-
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
