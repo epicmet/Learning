058f046f20ec15c704eb208af90dde56f5088632
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-29 18:11:27 +0330

    call back function

--- TypeScript/type-basics/app.ts
+++ TypeScript/type-basics/app.ts
@@ -11,3 +11,12 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 printResult(combineValues(8, 8));
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(1, 3, (res) => {
+  console.log(res);
+});
