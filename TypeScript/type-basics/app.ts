d5f98f181a3fdeebaf9d0134ee228c8019e9d2dd
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-29 18:22:38 +0330

    never type

--- TypeScript/type-basics/app.ts
+++ TypeScript/type-basics/app.ts
@@ -5,3 +5,9 @@ userInput = 5;
 userInput = "Matts";
 
 if (typeof userInput === "string") userName = userInput;
+
+function generateError(message: string, code: number): never {
+  throw { message, code };
+}
+
+generateError("something went wrong!", 500);
