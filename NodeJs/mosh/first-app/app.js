9a94c94a9777cc8fa33ced92f0c2c3326b70aa25
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-30 21:04:41 +0330

    some built in modules in node

--- NodeJs/mosh/first-app/app.js
+++ NodeJs/mosh/first-app/app.js
@@ -1,5 +1,25 @@
-const { log } = require("./logger");
+const os = require("os");
 
-log("hey");
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
 
-console.log(__filename);
+console.log(`Total Memory: ${totalMemory / 1e9}`);
+console.log(`Free Memory: ${freeMemory / 1e9}`);
+
+const fs = require("fs");
+
+fs.readdir("./", (err, files) => {
+  if (err) console.log("Error", err);
+  else console.log("Result", files);
+});
+
+const EventEmitter = require("events");
+
+const Logger = require("./logger");
+const logger = new Logger();
+
+logger.on("messageLogged", (arg) => {
+  console.log("Listener called", arg);
+});
+
+logger.log("message");
--- NodeJs/mosh/first-app/logger.js
+++ NodeJs/mosh/first-app/logger.js
@@ -1,8 +1,14 @@
+const EventEmitter = require("events");
+
+class Logger extends EventEmitter {
+  log(message) {
+    // Req to url
+    console.log(message);
+
+    this.emit("messageLogged", { id: 1, message: "hey buddy" });
+  }
+}
+
 const url = "http://randomurl.io";
 
-const log = (message) => {
-  // Req to url
-  console.log(message);
-};
-
-module.exports.log = log;
+module.exports = Logger;
