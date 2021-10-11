0ab5c33473a1c3801652c03cb9b42f5a8bb018ee
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-11 11:48:18 +0330

    simple REST

---
+++ NodeJs/mosh/RESTful-services/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3, 4]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
