5e93bb05efe48eb2807669cd3fcecec353985da8
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-11 12:57:41 +0330

    delete method

--- NodeJs/mosh/RESTful-services/index.js
+++ NodeJs/mosh/RESTful-services/index.js
@@ -21,16 +21,14 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("Could not find any course with given ID!");
+  if (!course)
+    return res.status(404).send("Could not find any course with given ID!");
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -42,15 +40,24 @@ app.post("/api/courses", (req, res) => {
 
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("Could not find any course with given ID!");
+  if (!course)
+    return res.status(404).send("Could not find any course with given ID!");
 
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("Could not find any course with given ID!");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
   res.send(course);
 });
 
