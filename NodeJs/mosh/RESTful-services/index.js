d7105a359c8ca129ade9a545bb82590cddbb50c3
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-11 12:12:03 +0330

    params and stuff

--- NodeJs/mosh/RESTful-services/index.js
+++ NodeJs/mosh/RESTful-services/index.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3, 4]);
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Could not find any course with given ID!");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
