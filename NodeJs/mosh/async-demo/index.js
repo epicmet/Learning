cfe365fcf9bfe76db2dfbc626bcb532c8335257c
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-18 14:55:19 +0330

    promise race

--- NodeJs/mosh/async-demo/index.js
+++ NodeJs/mosh/async-demo/index.js
@@ -1,10 +1,10 @@
 console.log("Before");
-getUser(1, (user) => {
-  console.log("User:", user);
-  getGithubRepos(user.name, (repos) => {
-    console.log(repos);
-  });
-});
+// getUser(1, (user) => {
+//   console.log("User:", user);
+//   getGithubRepos(user.name, (repos) => {
+//     console.log(repos);
+//   });
+// });
 console.log("After");
 
 function getUser(id, cb) {
@@ -20,3 +20,20 @@ function getGithubRepos(username, cb) {
     cb(["repo1", "repo2"]);
   }, 2000);
 }
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("P1");
+    resolve(1);
+  }, 2100);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("P2");
+    //resolve(2);
+    reject;
+  }, 2000);
+});
+
+Promise.race([p1, p2]).then((res) => console.log(res));
