fc5bde9d175d909e553f1a097d7638ebf5bf94d7
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-11-05 16:22:54 +0330

    delete and patch request

--- TypeScript/node-ts/src/routes/todos.ts
+++ TypeScript/node-ts/src/routes/todos.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, RequestHandler } from "express";
 import { Todo } from "../models/todo";
 
 const router = Router();
 
-const TODOS: Todo[] = [];
+let TODOS: Todo[] = [];
 
 router.get("/", (req: Request, res: Response) => {
   res.json({ res: TODOS });
@@ -15,6 +15,34 @@ router.post("/", (req: Request, res: Response) => {
   TODOS.push(newTodo);
 
   res.status(201).json({ message: "Created the todo", res: newTodo });
+});
+
+router.get("/:id", (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const foundedTodo = TODOS.filter((todo) => todo.id === id);
+  if (foundedTodo.length === 0)
+    return res.status(404).send("Did not found the TODO");
+
+  res.status(201).json({ messege: "Found it!", res: foundedTodo });
+});
+
+router.patch("/:id", (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const todoIndex = TODOS.findIndex((todo) => todo.id === id);
+  if (todoIndex === -1) return res.status(404).send("Did not found the TODO");
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, req.body.text);
+
+  res.json({ res: TODOS[todoIndex] });
+});
+
+router.delete("/:id", (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const todoIndex = TODOS.findIndex((todo) => todo.id === id);
+  if (todoIndex === -1) return res.status(404).send("Did not found the TODO");
+
+  TODOS.splice(todoIndex, 1);
+  res.json({ message: "deleted" });
 });
 
 export default router;
