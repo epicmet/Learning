956c62b1d53e1da6f0fbdca10bed54aa503f5a2d
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-11-09 14:55:42 +0330

    made server listen to messages with socket.io

--- NodeJs/rt-chat/app.js
+++ NodeJs/rt-chat/app.js
@@ -11,3 +11,30 @@ app.get("/", (req, res) => {
 });
 
 console.log("Server started");
+
+/// SocketIO
+const { idGenerator } = require("./utils");
+
+const SOCKET_LIST = {};
+
+const io = require("socket.io")(server);
+
+io.sockets.on("connection", (socket) => {
+  console.log("New user joined");
+  const newId = idGenerator();
+  SOCKET_LIST[newId] = socket;
+
+  socket.on("sendMsgToServer", (data) => {
+    console.log("someone send a message");
+
+    for (const key in SOCKET_LIST) {
+      SOCKET_LIST[key].emit("addToChat", data);
+    }
+  });
+
+  socket.on("disconnect", (socket) => {
+    delete SOCKET_LIST[socket.id];
+  });
+});
+
+server.listen(4141);
