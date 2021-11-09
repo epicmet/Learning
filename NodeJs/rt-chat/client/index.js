bb14cf8d6b44b0cce095c2c286c68d8c47ccd5f9
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-11-09 15:53:03 +0330

    added basic html and respnding real-time messages

---
+++ NodeJs/rt-chat/client/index.js
@@ -0,0 +1,20 @@
+const socket = io();
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("sendMsgToServer", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("addToChat", (msg) => {
+  const li = document.createElement("li");
+  li.textContent = msg;
+  messages.appendChild(li);
+  messages.scrollTop = messages.scrollHeight;
+});
