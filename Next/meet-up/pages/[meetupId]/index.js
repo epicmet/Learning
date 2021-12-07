7e711edd17f888aff106f28263503d14c2971267
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-12-07 17:58:14 +0330

    change fallback to blocking

--- Next/meet-up/pages/[meetupId]/index.js
+++ Next/meet-up/pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((m) => ({
       params: { meetupId: m._id.toString() },
     })),
