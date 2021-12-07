2640e76bc0a2a75fb5669a80d974dbb73fb489a9
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-12-07 16:36:57 +0330

    made api to save meetups in mongodb cluster

---
+++ Next/meet-up/pages/api/new-meetup.js
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://mahdi:mahdi@testcluster.xvbv2.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupCollection = db.collection("meetups");
+    const result = await meetupCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
