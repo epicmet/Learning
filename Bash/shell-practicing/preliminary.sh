9f2530371d62eb0a11977e1284e944b23d882c7f
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-12-13 11:30:54 +0330

    made use of code blocks for abstraction

--- Bash/shell-practicing/preliminary.sh
+++ Bash/shell-practicing/preliminary.sh
@@ -4,19 +4,17 @@ DATE=`date`
 LOGGED_USERS=`who`
 UPTIME=`uptime`
 
-echo -n "Todays date is : "
-echo $DATE
-
-echo -n "Logged in users: "
-echo $LOGGED_USERS
-
-echo -n "System Uptime is: "
-echo $UPTIME
-
 if [ ! -e "info.txt" ]; then
-  echo "Date: $DATE" >> info.txt
-  echo "Logged users: $LOGGED_USERS" >> info.txt
-  echo "Uptime: $UPTIME" >> info.txt
+  {
+    echo -n "Todays date is : "
+    echo $DATE
+
+    echo -n "Logged in users: "
+    echo $LOGGED_USERS
+
+    echo -n "System Uptime is: "
+    echo $UPTIME
+  } > info.txt  
 else 
   echo "Could not save information because there is already a file named info.txt in this directory"
   exit 1
