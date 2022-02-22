4df567cb2482cf5634770d4a742b56c4d6d3ffae
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-02-22 18:03:46 +0330

    Listen to online and offline event

--- service-workers/service-worker-exercise/web/js/blog.js
+++ service-workers/service-worker-exercise/web/js/blog.js
@@ -1,16 +1,29 @@
-(function Blog(){
-	"use strict";
+(function Blog() {
+  "use strict";
 
-	var offlineIcon;
-	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+  var offlineIcon;
+  var isOnline = "onLine" in navigator ? navigator.onLine : true;
+  var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+  document.addEventListener("DOMContentLoaded", ready, false);
 
+  // **********************************
 
-	// **********************************
+  function ready() {
+    offlineIcon = document.getElementById("connectivity-status");
 
-	function ready() {
-		offlineIcon = document.getElementById("connectivity-status");
-	}
+    if (!isOnline) {
+      offlineIcon.classList.remove("hidden");
+    }
 
+    window.addEventListener("online", function online() {
+      offlineIcon.classList.add("hidden");
+      isOnline = true;
+    });
+
+    window.addEventListener("offline", function offline() {
+      offlineIcon.classList.remove("hidden");
+      isOnline = false;
+    });
+  }
 })();
