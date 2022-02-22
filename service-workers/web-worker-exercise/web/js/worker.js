2817c7aa3f0c368fd6b6ee544f607b45ffbc2fd3
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-02-22 16:21:36 +0330

    Fibonacci numbers on a web worker

--- service-workers/web-worker-exercise/web/js/worker.js
+++ service-workers/web-worker-exercise/web/js/worker.js
@@ -2,13 +2,23 @@
 
 var curFib = 0;
 
-// TODO
-
+self.onmessage = onMessage;
 // **********************************
 
+function onMessage(evt) {
+  getNextFib();
+}
+
+function getNextFib() {
+  var fibNum = fib(curFib);
+  self.postMessage({ idx: curFib, fib: fibNum });
+  curFib++;
+  setTimeout(getNextFib, 0);
+}
+
 function fib(n) {
-	if (n < 2) {
-		return n;
-	}
-	return fib(n-1) + fib(n-2);
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
 }
