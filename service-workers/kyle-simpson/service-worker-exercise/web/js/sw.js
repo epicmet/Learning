ec4e42b93bbf69f1bd100bdc207974244fed7e84
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-02-23 17:45:31 +0330

    Caching is here baby

--- service-workers/kyle-simpson/service-worker-exercise/web/js/sw.js
+++ service-workers/kyle-simpson/service-worker-exercise/web/js/sw.js
@@ -1,8 +1,27 @@
 "use strict";
 
-const version = 2;
+const version = 4;
 var isOnline = true;
 var isLoggedIn = false;
+var cacheName = `ramblings-${version}`;
+
+var urlToCache = {
+  loggedOut: [
+    "/",
+    "/about",
+    "/contact",
+    "/404",
+    "/login",
+    "/offline",
+    "/css/style.css",
+    "/js/blog.js",
+    "/js/home.js",
+    "/js/login.js",
+    "/js/add-post.js",
+    "/images/logo.gif",
+    "/images/offline.png",
+  ],
+};
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
@@ -10,10 +29,11 @@ self.addEventListener("message", onMessage);
 
 main().catch(console.error);
 
-// ******************
+// *****************
 
 async function main() {
   await sendMessage({ requestStatusUpdate: true });
+  await cacheLoggedOutFiles();
 }
 
 async function onInstall() {
@@ -26,6 +46,8 @@ async function onActivate(e) {
 }
 
 async function handleActivation() {
+  await clearCaches();
+  await cacheLoggedOutFiles(true);
   await clients.claim();
   console.log(`Service worker (${version}), is activating ...`);
 }
@@ -48,4 +70,47 @@ function onMessage({ data }) {
       `Service worker (v${version}) status: isOnline: ${isOnline}, isLoggedIn: ${isLoggedIn}`
     );
   }
+}
+
+async function cacheLoggedOutFiles(forceReload = false) {
+  const cache = await caches.open(cacheName);
+
+  return Promise.all(
+    urlToCache.loggedOut.map(async (url) => {
+      let res;
+
+      if (!forceReload) {
+        res = cache.match(url);
+        if (res) return res;
+      }
+
+      res = await fetch(url, {
+        method: "GET",
+        credentials: "omit",
+        cache: "no-cache",
+      });
+
+      if (res.ok) {
+        await cache.put(url, res);
+      }
+    })
+  );
+}
+
+async function clearCaches() {
+  const cacheNames = await caches.keys();
+  const oldCacheNames = cacheNames.filter((cn) => {
+    if (/^ramblings-\d+$/.test(cn)) {
+      let [, cacheVersion] = cn.match(/^ramblings-(\d+)$/);
+      cacheVersion =
+        cacheVersion !== null ? Number(cacheVersion) : cacheVersion;
+      return cacheVersion > 0 && cacheVersion !== version;
+    }
+  });
+
+  return Promise.all(
+    oldCacheNames.map((cn) => {
+      return caches.delete(cn);
+    })
+  );
 }
