ff206083f2a50d389ad4fb59e191d8831f7e6600
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-04-12 15:05:14 +0430

    http2 wow

--- Real-time/fem-realtime-btholt/http2/exercise/backend/server.js
+++ Real-time/fem-realtime-btholt/http2/exercise/backend/server.js
@@ -29,11 +29,26 @@ const server = http2.createSecureServer({
   key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
 });
 
-/*
- *
- * Code goes here
- *
- */
+server.on("stream", (stream, headers) => {
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  if (path === "/msgs" && method === "GET") {
+    console.log("connected a stream " + stream.id);
+    stream.respond({
+      ":status": 200,
+      "content-type": "text/plain; charset=utf-8",
+    });
+
+    stream.write(JSON.stringify({ msg: getMsgs() }));
+    connections.push(stream);
+
+    stream.on("close", () => {
+      console.log("disconnected " + stream.id);
+      connections = connections.filter((s) => s !== stream);
+    });
+  }
+});
 
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
@@ -53,11 +68,13 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({ user, text, time: Date.now() });
+
+    res.end();
+
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msg: getMsgs() }));
+    });
   }
 });
 
--- Real-time/fem-realtime-btholt/http2/exercise/frontend/http2-chat.js
+++ Real-time/fem-realtime-btholt/http2/exercise/frontend/http2-chat.js
@@ -32,11 +32,42 @@ async function postNewMsg(user, text) {
 }
 
 async function getNewMsgs() {
-  /*
-   *
-   * code goes here
-   *
-   */
+  let reader;
+  const utf8Decoder = new TextDecoder("utf-8");
+
+  try {
+    const res = await fetch("/msgs");
+    reader = res.body.getReader();
+  } catch (e) {
+    console.error("connection error " + e);
+  }
+  presence.innerHTML = "🟢";
+
+  let readerResponse;
+  let done;
+  do {
+    try {
+      readerResponse = await reader.read();
+    } catch (e) {
+      console.error("reader error " + e);
+      presence.innerHTML = "🔴";
+      return;
+    }
+
+    const chunk = utf8Decoder.decode(readerResponse.value, { stream: true });
+    done = readerResponse.done;
+
+    if (chunk) {
+      try {
+        const json = JSON.parse(chunk);
+        allChat = json.msg;
+        render();
+      } catch (e) {
+        console.error("parse error " + e);
+      }
+    }
+  } while (!done);
+  presence.innerHTML = "🔴";
 }
 
 function render() {
