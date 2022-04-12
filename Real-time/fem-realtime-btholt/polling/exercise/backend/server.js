7793e1579459ae2f4067ebb4b3ef8fce65028061
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-04-12 14:04:14 +0430

    Polling

---
+++ Real-time/fem-realtime-btholt/polling/exercise/backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import nanobuffer from "nanobuffer";
+import morgan from "morgan";
+
+// set up a limited array
+const msg = new nanobuffer(50);
+const getMsgs = () => Array.from(msg).reverse();
+
+// feel free to take out, this just seeds the server with at least one message
+msg.push({
+  user: "brian",
+  text: "hi",
+  time: Date.now(),
+});
+
+// get express ready to run
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(express.static("frontend"));
+
+app.get("/poll", function (req, res) {
+  res.json({ msg: getMsgs() });
+});
+
+app.post("/poll", function (req, res) {
+  const { user, text } = req.body;
+
+  msg.push({ user, text, date: Date.now() });
+
+  res.json({ status: "ok" });
+});
+
+// start the server
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`listening on http://localhost:${port}`);
---
+++ Real-time/fem-realtime-btholt/polling/exercise/frontend/polling-chat.js
@@ -0,0 +1,74 @@
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+
+// let's store all current messages here
+let allChat = [];
+
+// the interval to poll at in milliseconds
+const INTERVAL = 3000;
+
+// a submit listener on the form in the HTML
+chat.addEventListener("submit", function (e) {
+  e.preventDefault();
+  postNewMsg(chat.elements.user.value, chat.elements.text.value);
+  chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+  const data = { user, text };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  };
+
+  await fetch("/poll", options);
+}
+
+async function getNewMsgs() {
+  let json;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+
+    if (res.status >= 400) {
+      throw new Error("request did not succeed: " + res.status);
+    }
+
+    allChat = json.msg;
+    render();
+    failedTries = 0;
+  } catch (e) {
+    console.error("polling error", e);
+    failedTries++;
+  }
+}
+
+function render() {
+  // as long as allChat is holding all current messages, this will render them
+  // into the ui. yes, it's inefficent. yes, it's fine for this example
+  const html = allChat.map(({ user, text, time, id }) =>
+    template(user, text, time, id)
+  );
+  msgs.innerHTML = html.join("\n");
+}
+
+// given a user and a msg, it returns an HTML string to render to the UI
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+
+const BACKOFF = 5000;
+let timeToMakeNextRequest = 0;
+let failedTries = 0;
+
+async function rafTimer(time) {
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL + failedTries * BACKOFF;
+  }
+
+  requestAnimationFrame(rafTimer);
+}
+
+requestAnimationFrame(rafTimer);
