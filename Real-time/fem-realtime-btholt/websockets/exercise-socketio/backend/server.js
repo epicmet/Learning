25ae638b6cb4ffd97f5cb7e1d71ed58242c11724
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-04-12 16:58:03 +0430

    Socket.io

--- Real-time/fem-realtime-btholt/websockets/exercise-socketio/backend/server.js
+++ Real-time/fem-realtime-btholt/websockets/exercise-socketio/backend/server.js
@@ -19,11 +19,23 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * Code goes here
- *
- */
+const io = new Server(server, {});
+
+io.on("connection", (socket) => {
+  console.log("connected " + socket.id);
+
+  io.emit("msg:get", { msg: getMsgs() });
+
+  socket.on("disconnect", (socket) => {
+    console.log("disconnected " + socket.id);
+  });
+
+  socket.on("msg:post", (data) => {
+    msg.push({ user: data.user, text: data.text, time: Date.now() });
+
+    io.emit("msg:get", { msg: getMsgs() });
+  });
+});
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
--- Real-time/fem-realtime-btholt/websockets/exercise-socketio/frontend/socketio-chat.js
+++ Real-time/fem-realtime-btholt/websockets/exercise-socketio/frontend/socketio-chat.js
@@ -5,11 +5,21 @@ const msgs = document.getElementById("msgs");
 const presence = document.getElementById("presence-indicator");
 let allChat = [];
 
-/*
- *
- * Code goes here
- *
- */
+const socket = io("http://localhost:8080");
+
+socket.on("connect", () => {
+  console.log("connected");
+  presence.innerHTML = "🟢";
+});
+
+socket.on("disconnect", () => {
+  presence.innerHTML = "🔴";
+});
+
+socket.on("msg:get", (data) => {
+  allChat = data.msg;
+  render();
+});
 
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -18,11 +28,9 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  /*
-   *
-   * Code goes here
-   *
-   */
+  const data = { user, text };
+
+  socket.emit("msg:post", data);
 }
 
 function render() {
