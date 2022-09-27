ccad2f52da2ae8237a15b1dfa73dbdc912c54bf6
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-09-27 19:15:41 +0330

    MoodTracker: Analytics

--- ReactNative/MoodTracker/src/screens/Analytics.screen.tsx
+++ ReactNative/MoodTracker/src/screens/Analytics.screen.tsx
@@ -1,9 +1,44 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { groupBy } from 'lodash';
+import { VictoryPie } from 'victory-native';
+import { useAppContext } from '../App.provider';
+import { theme } from '../theme';
 
 export const Analytics: React.FC = () => {
+  const { moodList } = useAppContext();
+
+  const data = Object.entries(groupBy(moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Analytics</Text>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
