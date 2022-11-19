b701df4de39988f3fd159630c286d1a8d2cf68ee
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-11-19 10:54:43 +0330

    changing threadpool

--- NodeJs/advanced-node/threads.cjs
+++ NodeJs/advanced-node/threads.cjs
@@ -1,3 +1,6 @@
+// Changing number of libuv`s threads in thread pool.
+process.env.UV_THREADPOOL_SIZE = 5;
+
 const crypto = require("crypto");
 
 const start = Date.now();
@@ -17,6 +20,7 @@ crypto.pbkdf2("a", "b", 100_000, 512, "sha512", () => {
   console.log("4:", Date.now() - start);
 })
 
+// (Without changing default behavior of libuv`s threadpool) 
 // This call would take more than 4 above! Because libuv`s thread pool has 4 threads.
 crypto.pbkdf2("a", "b", 100_000, 512, "sha512", () => {
   console.log("5:", Date.now() - start);
