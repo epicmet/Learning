378a61fc1ec621d7e4d868eb52a910e49716c60f
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-11-19 17:01:02 +0330

    Node clusters

--- NodeJs/advanced-node/index.cjs
+++ NodeJs/advanced-node/index.cjs
@@ -1,16 +1,24 @@
-const express = require("express");
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require("cluster");
 
-const app = express();
+if (cluster.isPrimary) {
+  cluster.fork();
+  cluster.fork();
+} else {
+  const express = require("express");
+  const crypto = require("crypto");
 
-function doWork(duration) {
-  const start = Date.now();
+  const app = express();
 
-  while (Date.now() - start < duration) {}
+  app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 100_000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast");
+  });
+
+  app.listen(3004);
 }
-
-app.get("/", (req, res) => {
-  doWork(5000);
-  res.send("Hi there");
-});
-
-app.listen(3004);
