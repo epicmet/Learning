caaafe6e7449843f14f91c811457a131bcab648d
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-01-10 18:00:47 +0330

    First Nestjs commit

---
+++ NestJS/scratch/src/main.ts
@@ -0,0 +1,22 @@
+import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+@Controller()
+class AppController {
+  @Get()
+  getRootRout() {
+    return "Hi there";
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule { }
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+}
+bootstrap();
