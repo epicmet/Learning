93597270460fb13173a9f733694b89b998aa18a6
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-09-18 14:24:14 +0330

    (Playground:java) Validate data before using them

--- Playgrounds/java/mortgage-calculator/src/Main.java
+++ Playgrounds/java/mortgage-calculator/src/Main.java
@@ -6,21 +6,41 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterestPercentage = 0;
+        int numberOfPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello and welcome!");
 
-        System.out.print("Principal: ");
-        double principal = Double.parseDouble(scanner.nextLine().trim());
+        while(true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if(principal >= 1_000 && principal <= 1_000_000) {
+                break;
+            }
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
 
+        while(true) {
+            System.out.print("Annual Interest rate: ");
+            float annualInterestRate = scanner.nextFloat();
+            if(annualInterestRate >= 1 && annualInterestRate <= 30){
+                monthlyInterestPercentage = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
 
-        System.out.print("Annual Interest rate: ");
-        double annualInterestRate = Double.parseDouble(scanner.nextLine().trim());
-        double monthlyInterestPercentage = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.print("Period (Years): ");
-        int periodInYears = Integer.parseInt(scanner.nextLine().trim());
-        int numberOfPayments = periodInYears * 12;
+        while(true) {
+            System.out.print("Period (Years): ");
+            byte periodInYears = scanner.nextByte();
+            if(periodInYears >= 1 && periodInYears <= 30) {
+                numberOfPayments = periodInYears * 12;
+                break;
+            }
+        }
 
         double up = monthlyInterestPercentage * Math.pow(1 + monthlyInterestPercentage, numberOfPayments);
         double down = Math.pow(1 + monthlyInterestPercentage, numberOfPayments) - 1;
